d117225c3d98e6d6c83e8dce0bc2b6bddd25cbde
Hugo Franco <hfranco21@hotmail.com>
2021-06-01 16:49:31 -0500

    start project

---
+++ src/Production.java
@@ -0,0 +1,11 @@
+public class Production {
+    private String leftSide;
+    private String[] rightSide;
+
+    public Production(){}
+
+    public Production(String leftSide, String[] rightSide){
+        this.leftSide = leftSide;
+        this.rightSide = rightSide;
+    }
+}
